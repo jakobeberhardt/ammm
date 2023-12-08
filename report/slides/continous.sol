f2c1c2cbf1cb96fcf4a30018b559105eb4162120
jakobeberhardt <jakob.eberhardt@mail.de>
2023-12-08 15:29:52 +0100

    Adds report sources

---
+++ report/slides/continous.sol
@@ -0,0 +1,8 @@
+x = [1
+         0 1 1];
+end = [2 -1 1 4];
+start = [1 0 1 2];
+y = [[0 0 1 1] <---- 1 needts to be delivered earlier 
+     [0 0 0 0]
+     [1 0 0 0]
+     [0 1 1 1]];
---
+++ report/slides/feasable.sol
@@ -0,0 +1,19 @@
+// solution (optimal) with objective 10
+// Quality Incumbent solution:
+// MILP objective                                 1.0000000000e+01
+// MILP solution norm |x| (Total, Max)            4.60000e+01  4.00000e+00
+// MILP solution error (Ax=b) (Total, Max)        0.00000e+00  0.00000e+00
+// MILP x bound error (Total, Max)                0.00000e+00  0.00000e+00
+// MILP x integrality error (Total, Max)          0.00000e+00  0.00000e+00
+// MILP slack bound error (Total, Max)            0.00000e+00  0.00000e+00
+// MILP indicator slack bound error (Total, Max)  0.00000e+00  0.00000e+00
+// 
+
+x = [1
+         0 1 1];
+end = [2 1 1 4];
+start = [1 2 1 2];
+y =         [[1 1 0 0]
+             [0 0 0 0]
+             [1 0 0 0]
+             [0 1 1 1]];
